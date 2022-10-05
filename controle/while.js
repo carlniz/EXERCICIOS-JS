5d03a3f3a84d7d08fccb8ee65d5c54b6edcad0ba
Carlos Lock <emaildolock@gmail.com>
2022-10-05 01:32:04 -0300

    teste

--- controle/while.js
+++ controle/while.js
@@ -7,6 +7,6 @@ let opcao = 0;
 //while trabalha encima de condição verdadeira, quando for falsa, ele sai do while  
 while(opcao != -1){
     opcao = getInteiroAleatorioEntre(-1, 10);
-    console.log(`Opção escolhida foi ${opcao}.`);
+    console.log(`Opção escolhida foi essa ${opcao}.`);
 }
 
